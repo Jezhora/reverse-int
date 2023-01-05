135f65c9bfdbc70ab710d8ca508cc32caded1495
jezhora <jezhora.photo@gmail.com>
2023-01-05 14:57:45 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    const nS = Math.abs(n).toString();
+    let res = '';
+    
+
+    while (i < nS.length) {
+        res = `${nS[i]}${res}`
+        i += 1;
+  }
+  let result = Number(res);
+  return result;
 }
+  
+
